4c709cddd62c3d7ba390fd7a22589a3bb9be4388
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2024-01-03 16:38:10 +0200

    Update Orb View
    Removed view updating

--- Assets/Resources/Scripts/View/OrbView.cs
+++ Assets/Resources/Scripts/View/OrbView.cs
@@ -6,11 +6,5 @@ namespace View
     public class OrbView : MonoBehaviour
     {
         private Orb _model;
-
-        private void Update()
-        {
-            transform.position = _model.Position;
-            transform.rotation = Quaternion.Euler(0, 0, _model.Rotation);
-        }
     }
 }
