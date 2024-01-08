c46238aefdd9faba882c97450e6372bf55cd88e9
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2024-01-08 12:24:19 +0200

    Add orb pool
    Implementing of object pool

---
+++ Assets/Resources/Scripts/Model/OrbPool.cs
@@ -0,0 +1,49 @@
+﻿using View;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace Model
+{
+    public class OrbPool
+    {
+        private ObjectPool<OrbView> _pool;
+        private OrbView _prefab;
+
+        public OrbPool(OrbView prefab, int prewarmedOrbsCount)
+        {
+            _prefab = prefab;
+            _pool = new ObjectPool<OrbView>(OnCreateOrb, OnGetOrb, OnRelease, OnOrbDestroy, false, prewarmedOrbsCount);
+        }
+
+        public OrbView Get()
+        {
+            var orb = _pool.Get();
+            return orb;
+        }
+
+        public void Release(OrbView orb)
+        {
+            _pool.Release(orb);
+        }
+
+        private void OnOrbDestroy(OrbView orb)
+        {
+            Object.Destroy(orb);
+        }
+
+        private void OnRelease(OrbView orb)
+        {
+            orb.gameObject.SetActive(false);
+        }
+
+        private void OnGetOrb(OrbView orb)
+        {
+            orb.gameObject.SetActive(true);
+        }
+
+        private OrbView OnCreateOrb()
+        {
+            return Object.Instantiate(_prefab);
+        }
+    }
+}
