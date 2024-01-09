af0f99b1bb93923b606838405983dfecd3bf3304
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2024-01-09 18:53:32 +0200

    Add bullet

---
+++ Assets/Resources/Scripts/Model/Bullet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Model
+{
+    public class Bullet : Transformable
+    {
+        public Bullet(Vector2 position, float rotation) : base(position, rotation) { }
+    }
+}
---
+++ Assets/Resources/Scripts/View/BulletView.cs
@@ -0,0 +1,21 @@
+﻿using Model;
+using UnityEngine;
+
+namespace View
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BulletView : MonoBehaviour
+    {
+        private Bullet _model;
+        private Rigidbody2D _rigitBody;
+
+        public void Initialize(Vector2 direction, float rotation, float movingSpeed)
+        {
+            _model = new Bullet(transform.position, rotation);
+            transform.rotation = Quaternion.Euler(0, 0, _model.Rotation);
+
+            _rigitBody = GetComponent<Rigidbody2D>();
+            _rigitBody.velocity = direction * movingSpeed;
+        }
+    }
+}
